1e80289c61cbfdafeb18d92f97dcc6bc0023e613
Przemek <przemek@przemyk.xyz>
2020-11-20 08:17:52 +0100

    added magnetic separator jei compat

--- src/main/java/xyz/przemyk/real_minerals/compat/jei/RealMineralsJEIPlugin.java
+++ src/main/java/xyz/przemyk/real_minerals/compat/jei/RealMineralsJEIPlugin.java
@@ -12,6 +12,8 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import xyz.przemyk.real_minerals.init.RealMinerals;
 import xyz.przemyk.real_minerals.init.Registering;
+import xyz.przemyk.real_minerals.machines.electric.magnetic_separator.MagneticSeparatorContainer;
+import xyz.przemyk.real_minerals.machines.electric.magnetic_separator.MagneticSeparatorScreen;
 import xyz.przemyk.real_minerals.machines.electric.magnetizer.MagnetizerContainer;
 import xyz.przemyk.real_minerals.machines.electric.magnetizer.MagnetizerScreen;
 import xyz.przemyk.real_minerals.machines.not_electric.alloy_furnace.AlloyFurnaceContainer;
@@ -27,6 +29,7 @@ public class RealMineralsJEIPlugin implements IModPlugin {
     public static final ResourceLocation CRUSHER_CATEGORY_ID = new ResourceLocation(RealMinerals.MODID, "crushing");
     public static final ResourceLocation ALLOY_CATEGORY_ID = new ResourceLocation(RealMinerals.MODID, "alloying");
     public static final ResourceLocation MAGNETIZER_CATEGORY_ID = new ResourceLocation(RealMinerals.MODID, "magnetizer");
+    public static final ResourceLocation MAGNETIC_SEPARATOR_CATEGORY_ID = new ResourceLocation(RealMinerals.MODID, "magnetic_separator");
 
     public RealMineralsJEIPlugin() {
 
@@ -40,7 +43,12 @@ public class RealMineralsJEIPlugin implements IModPlugin {
     @Override
     public void registerCategories(IRecipeCategoryRegistration registration) {
         IGuiHelper guiHelper = registration.getJeiHelpers().getGuiHelper();
-        registration.addRecipeCategories(new CrusherRecipeCategory(guiHelper), new AlloyRecipeCategory(guiHelper), new MagnetizerRecipeCategory(guiHelper));
+        registration.addRecipeCategories(
+                new CrusherRecipeCategory(guiHelper),
+                new AlloyRecipeCategory(guiHelper),
+                new MagnetizerRecipeCategory(guiHelper),
+                new MagneticSeparatorRecipeCategory(guiHelper)
+        );
     }
 
     @SuppressWarnings("ConstantConditions")
@@ -50,6 +58,7 @@ public class RealMineralsJEIPlugin implements IModPlugin {
         registration.addRecipes(RealMinerals.getAllRecipes(world, RealMinerals.CRUSHER_RECIPE_TYPE), CRUSHER_CATEGORY_ID);
         registration.addRecipes(RealMinerals.getAllRecipes(world, RealMinerals.ALLOY_RECIPE_TYPE), ALLOY_CATEGORY_ID);
         registration.addRecipes(RealMinerals.getAllRecipes(world, RealMinerals.MAGNETIZER_RECIPE_TYPE), MAGNETIZER_CATEGORY_ID);
+        registration.addRecipes(RealMinerals.getAllRecipes(world, RealMinerals.MAGNETIC_SEPARATOR_RECIPE_TYPE), MAGNETIC_SEPARATOR_CATEGORY_ID);
     }
 
     @Override
@@ -61,6 +70,8 @@ public class RealMineralsJEIPlugin implements IModPlugin {
         registration.addRecipeTransferHandler(AlloyFurnaceContainer.class, VanillaRecipeCategoryUid.FUEL, 1, 1, 7, 36);
 
         registration.addRecipeTransferHandler(MagnetizerContainer.class, MAGNETIZER_CATEGORY_ID, 0, 1, 2, 36);
+
+        registration.addRecipeTransferHandler(MagneticSeparatorContainer.class, MAGNETIC_SEPARATOR_CATEGORY_ID, 0, 1, 3, 36);
     }
 
     @Override
@@ -68,6 +79,9 @@ public class RealMineralsJEIPlugin implements IModPlugin {
         registration.addRecipeCatalyst(new ItemStack(Registering.CRUSHER_BLOCK.ITEM.get()), CRUSHER_CATEGORY_ID, VanillaRecipeCategoryUid.FUEL);
         registration.addRecipeCatalyst(new ItemStack(Registering.ALLOY_FURNACE_BLOCK.ITEM.get()), ALLOY_CATEGORY_ID, VanillaRecipeCategoryUid.FUEL);
         registration.addRecipeCatalyst(new ItemStack(Registering.MAGNETIZER_BLOCK.ITEM.get()), MAGNETIZER_CATEGORY_ID);
+        registration.addRecipeCatalyst(new ItemStack(Registering.MAGNETIC_SEPARATOR_BLOCK.ITEM.get()), MAGNETIC_SEPARATOR_CATEGORY_ID);
+        registration.addRecipeCatalyst(new ItemStack(Registering.ELECTRIC_FURNACE_BLOCK.ITEM.get()), VanillaRecipeCategoryUid.FURNACE);
+        registration.addRecipeCatalyst(new ItemStack(Registering.BURNING_GENERATOR_BLOCK.ITEM.get()), VanillaRecipeCategoryUid.FUEL);
     }
 
     @Override
@@ -75,5 +89,6 @@ public class RealMineralsJEIPlugin implements IModPlugin {
         registration.addRecipeClickArea(CrusherScreen.class, 78, 32, 28, 23, CRUSHER_CATEGORY_ID, VanillaRecipeCategoryUid.FUEL);
         registration.addRecipeClickArea(AlloyFurnaceScreen.class, 78, 32, 28, 23, ALLOY_CATEGORY_ID, VanillaRecipeCategoryUid.FUEL);
         registration.addRecipeClickArea(MagnetizerScreen.class, 78, 32, 28, 23, MAGNETIZER_CATEGORY_ID);
+        registration.addRecipeClickArea(MagneticSeparatorScreen.class, 65, 26, 33, 34, MAGNETIC_SEPARATOR_CATEGORY_ID);
     }
 }
