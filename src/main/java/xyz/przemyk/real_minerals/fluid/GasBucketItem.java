04801ca732795cb726a354127920effd430c0c5a
Przemek <przemek@przemyk.xyz>
2020-12-06 10:58:27 +0100

    fixed gas bucket not having fluid capability

--- src/main/java/xyz/przemyk/real_minerals/fluid/GasBucketItem.java
+++ src/main/java/xyz/przemyk/real_minerals/fluid/GasBucketItem.java
@@ -3,12 +3,16 @@ package xyz.przemyk.real_minerals.fluid;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.BucketItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper;
 
 import javax.annotation.Nullable;
 import java.util.function.Supplier;
@@ -27,5 +31,10 @@ public class GasBucketItem extends BucketItem {
             worldIn.addParticle(ParticleTypes.LARGE_SMOKE, (double)posIn.getX() + Math.random(), (double)posIn.getY() + Math.random(), (double)posIn.getZ() + Math.random(), 0.0D, 0.0D, 0.0D);
         }
         return true;
+    }
+
+    @Override
+    public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable CompoundNBT nbt) {
+        return new FluidBucketWrapper(stack);
     }
 }
