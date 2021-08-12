70ec0b887e0078417a232bfb0fd42f8ba3faf69b
Przemyk <przemek@przemyk.xyz>
2021-08-12 17:59:01 +0200

    transferring fluids by clicking with an item on a machine

--- src/main/java/xyz/przemyk/real_minerals/blocks/MachineBlock.java
+++ src/main/java/xyz/przemyk/real_minerals/blocks/MachineBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
@@ -17,8 +18,12 @@ import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 import javax.annotation.Nullable;
 import java.util.function.Supplier;
@@ -63,13 +68,65 @@ public class MachineBlock extends Block {
     public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
         if (!worldIn.isRemote) {
             TileEntity tileEntity = worldIn.getTileEntity(pos);
-            if (tileEntity instanceof INamedContainerProvider) {
-                NetworkHooks.openGui((ServerPlayerEntity) player, (INamedContainerProvider) tileEntity, pos);
+            if (tileEntity != null) {
+                ItemStack itemStack = player.getHeldItem(handIn);
+                Direction face = hit.getFace();
+                if (tryExtractFluid(worldIn, pos, player, handIn, tileEntity, itemStack, face)) {
+                    return ActionResultType.CONSUME;
+                }
+
+                if (tileEntity instanceof INamedContainerProvider) {
+                    NetworkHooks.openGui((ServerPlayerEntity) player, (INamedContainerProvider) tileEntity, pos);
+                }
             }
             return ActionResultType.CONSUME;
         }
         return ActionResultType.SUCCESS;
     }
+
+    private static boolean tryExtractFluid(World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, TileEntity tileEntity, ItemStack itemStack, Direction face) {
+        if (!itemStack.isEmpty()) {
+            ItemStack copy = ItemHandlerHelper.copyStackWithSize(itemStack, 1);
+            copy.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY).ifPresent(itemFluidHandler ->
+                    tileEntity.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, face).ifPresent(tileFluidHandler -> {
+                FluidStack transferred = tryTransfer(itemFluidHandler, tileFluidHandler, Integer.MAX_VALUE);
+                if (!transferred.isEmpty()) {
+                    worldIn.playSound(null, pos, transferred.getFluid().getAttributes().getEmptySound(transferred), SoundCategory.BLOCKS, 1.0f, 1.0f);
+                } else {
+                    transferred = tryTransfer(tileFluidHandler, itemFluidHandler, Integer.MAX_VALUE);
+                    if (!transferred.isEmpty()) {
+                        worldIn.playSound(null, pos, transferred.getFluid().getAttributes().getFillSound(transferred), SoundCategory.BLOCKS, 1.0f, 1.0f);
+                    }
+                }
+
+                if (!transferred.isEmpty()) {
+                    player.setHeldItem(handIn, DrinkHelper.fill(itemStack, player, itemFluidHandler.getContainer()));
+                }
+            }));
+            return copy.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY).isPresent();
+        }
+        return false;
+    }
+
+    public static FluidStack tryTransfer(IFluidHandler input, IFluidHandler output, int maxFill) {
+        // first, figure out how much we can drain
+        FluidStack simulated = input.drain(maxFill, IFluidHandler.FluidAction.SIMULATE);
+        if (!simulated.isEmpty()) {
+            // next, find out how much we can fill
+            int simulatedFill = output.fill(simulated, IFluidHandler.FluidAction.SIMULATE);
+            if (simulatedFill > 0) {
+                // actually drain
+                FluidStack drainedFluid = input.drain(simulatedFill, IFluidHandler.FluidAction.EXECUTE);
+                if (!drainedFluid.isEmpty()) {
+                    // actually fill
+                    output.fill(drainedFluid.copy(), IFluidHandler.FluidAction.EXECUTE);
+                }
+                return drainedFluid;
+            }
+        }
+        return FluidStack.EMPTY;
+    }
+
 
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
